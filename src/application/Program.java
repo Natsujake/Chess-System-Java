3a2f1f5684ecf3b4ff795ba02851d99ebd2edaea
Natsujake <gabrielnatsujake@gmail.com>
2022-07-03 20:07:23 -0300

    Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,8 +41,12 @@ public class Program {
 				
 				if (chessMatch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
-					type = type.toUpperCase();
+					String type = sc.nextLine().toUpperCase();;
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.println("Invalid value! Try again!");
+						System.out.print("Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();;
+					}
 					chessMatch.replacePromotedPiece(type);
 				}
 				
